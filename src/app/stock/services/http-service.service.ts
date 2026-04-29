f4f67b680f7ff6106503017f6b765146032d7180
khoaIT <anhkhoale2406@gmail.com>
2026-04-29 23:27:12 +0700

    fix: responsive mobile cho tất cả các trang lần 2

--- src/app/stock/services/http-service.service.ts
+++ src/app/stock/services/http-service.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class HttpServiceService {
-  // private REST_API_SERVER = 'https://stock-market-backend-production-6d5f.up.railway.app/api';
-  private REST_API_SERVER = 'http://localhost:3000';
+  private REST_API_SERVER = 'https://stock-market-backend-production-6d5f.up.railway.app/api';
+  // private REST_API_SERVER = 'http://localhost:3000';
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
